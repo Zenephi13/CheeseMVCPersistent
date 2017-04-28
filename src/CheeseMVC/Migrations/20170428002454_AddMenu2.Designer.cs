0b20c45c6eb00c108810f3d29a083136a92f8974
Jordan Smith <zenephi13@yahoo.com>
2017-04-27 21:25:41 -0500

    Final working version

--- src/CheeseMVC/Migrations/20170428002454_AddMenu2.Designer.cs
+++ src/CheeseMVC/Migrations/20170428002454_AddMenu2.Designer.cs
@@ -8,9 +8,10 @@ using CheeseMVC.Data;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170428002454_AddMenu2")]
+    partial class AddMenu2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
---
+++ src/CheeseMVC/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCategoryViewModel
+    {
+        [Required]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        public AddCategoryViewModel()
+        {
+
+        }
+    }
+}
